14296f76c1802dff3345f94d7aa1c6e622b39d36
Santiago Sierra <santiago.sierra@kiwibot.com>
2022-07-27 16:48:26 -0500

    Inclusion de funciones

--- js/simulador.js
+++ js/simulador.js
@@ -1,22 +1,50 @@
-alert("Bienvenidos a la calculadora de promedios ponderados")
+// alert("Bienvenidos a la calculadora de promedios ponderados")
 // debugger
-let cantMaterias=parseInt(prompt("Ingrese la cantidad de materias: "))
+// let cantMaterias=parseInt(prompt("Ingrese la cantidad de materias: "))
 
+// let creditosTotales=0;
+// let suma=0;
+// let promedioPonderado=0;
+// for (let i=1; i<=cantMaterias;i++){
+//     let nota=[];
+//     let creditos=[];
+//     let ponderada=[];
+
+//     nota[i]=parseFloat(prompt("Ingrese la nota de la materia "+ i))
+//     creditos[i]=parseInt(prompt("Ingrese el número de créditos de la materia "+ i))
+//     ponderada[i]=nota[i]*creditos[i]
+//     creditosTotales=creditosTotales+creditos[i]
+//     suma=suma+ponderada[i]
+//     console.log(ponderada[i])
+// }   
+// promedioPonderado=suma/creditosTotales
+// alert("Su promedio ponderado es: "+ promedioPonderado)
+
+alert("Bienvenidos a la calculadora de promedios ponderados")
+let cantMaterias=parseInt(prompt("Ingrese la cantidad de materias: "))
+let nota=0;
+let creditos=0;
+let ponderada=0;
 let creditosTotales=0;
 let suma=0;
-let promedioPonderado=0;
-for (let i=1; i<=cantMaterias;i++){
-    let nota=[];
-    let creditos=[];
-    let ponderada=[];
 
-    nota[i]=parseFloat(prompt("Ingrese la nota de la materia "+ i))
-    creditos[i]=parseInt(prompt("Ingrese el número de créditos de la materia "+ i))
-    ponderada[i]=nota[i]*creditos[i]
-    creditosTotales=creditosTotales+creditos[i]
-    suma=suma+ponderada[i]
-    console.log(ponderada[i])
-}   
+function promedio(not, credito){
+    ponderada=nota*creditos
+    creditosTotales=creditosTotales+creditos
+    suma=suma+ponderada
+    console.log(ponderada)
+}
+
+for (let i=1; i<=cantMaterias;i++){
+    nota=0;
+    creditos=0;
+    ponderada=0;
+
+    nota=parseFloat(prompt("Ingrese la nota de la materia "+ i))
+    debugger
+    creditos=parseInt(prompt("Ingrese el número de créditos de la materia "+ i))
+    promedio(nota,creditos);
+}
+
 promedioPonderado=suma/creditosTotales
 alert("Su promedio ponderado es: "+ promedioPonderado)
-
